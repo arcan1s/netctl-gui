814f2c79048ef07c848e327372ef70d78a35611f
arcan1s <esalexeev@gmail.com>
2014-08-25 20:29:30 +0400

    update license agreement to trdparty items

--- sources/3rdparty/language/language.cpp
+++ sources/3rdparty/language/language.cpp
@@ -1,18 +1,18 @@
 /***************************************************************************
- *   This file is part of netctl-gui                                       *
+ *  Copyright (C) 2014  Evgeniy Alekseev                                      *
  *                                                                         *
- *   netctl-gui is free software: you can redistribute it and/or           *
- *   modify it under the terms of the GNU General Public License as        *
- *   published by the Free Software Foundation, either version 3 of the    *
- *   License, or (at your option) any later version.                       *
+ *  This library is free software; you can redistribute it and/or          *
+ *  modify it under the terms of the GNU Lesser General Public             *
+ *  License as published by the Free Software Foundation; either           *
+ *  version 3.0 of the License, or (at your option) any later version.     *
  *                                                                         *
- *   netctl-gui is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
+ *  This library is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *  Lesser General Public License for more details.                        *
  *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with netctl-gui. If not, see http://www.gnu.org/licenses/       *
+ *  You should have received a copy of the GNU Lesser General Public       *
+ *  License along with this library.                                       *
  ***************************************************************************/
 
 
@@ -95,7 +95,7 @@ QStringList Language::getAvailableLanguages()
 {
     QStringList languages;
     languages.append(QString("en"));
-    languages.append(QString("ru"));
+    // put your languages here
 
     return languages;
 }
--- sources/3rdparty/language/language.h
+++ sources/3rdparty/language/language.h
@@ -1,18 +1,18 @@
 /***************************************************************************
- *   This file is part of netctl-gui                                       *
+ *  Copyright (C) 2014  Evgeniy Alekseev                                      *
  *                                                                         *
- *   netctl-gui is free software: you can redistribute it and/or           *
- *   modify it under the terms of the GNU General Public License as        *
- *   published by the Free Software Foundation, either version 3 of the    *
- *   License, or (at your option) any later version.                       *
+ *  This library is free software; you can redistribute it and/or          *
+ *  modify it under the terms of the GNU Lesser General Public             *
+ *  License as published by the Free Software Foundation; either           *
+ *  version 3.0 of the License, or (at your option) any later version.     *
  *                                                                         *
- *   netctl-gui is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
+ *  This library is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+ *  Lesser General Public License for more details.                        *
  *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with netctl-gui. If not, see http://www.gnu.org/licenses/       *
+ *  You should have received a copy of the GNU Lesser General Public       *
+ *  License along with this library.                                       *
  ***************************************************************************/
 
 #ifndef LANGUAGE_H
