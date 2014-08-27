fc3956caf1c0deee508ff52ac7b65df283418d6f
arcan1s <darkarcanis@mail.ru>
2014-08-27 17:07:00 +0400

    ya submodules fix

---
+++ sources/3rdparty/language-config.h
@@ -0,0 +1,12 @@
+#ifndef LANGUAGE_CONFIG_H
+#define LANGUAGE_CONFIG_H
+
+
+// put your languages here comma separated
+// the first language is default language
+#define LANGUAGES "en,ru"
+// language key in the configuration file
+#define LANGUAGE_KEY "LANGUAGE"
+
+
+#endif /* LANGUAGE_CONFIG_H */
