835e82ca639d62cf336671e1c0012bdad67cd9fc
NOGISAKA Sadata <ngsksdt@gmail.com>
2014-09-10 21:20:41 +0900

    translated netctlgui-helper to Japanese

---
+++ sources/resources/translations-helper/ja.ts
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>NetctlHelper</name>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="28"/>
+        <source>Unknown flag
+</source>
+        <translation>不明なフラグ</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="54"/>
+        <source>Usage:</source>
+        <translation>使用法：</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="56"/>
+        <source>Options:</source>
+        <translation>オプション：</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="59"/>
+        <source>read configuration from this file</source>
+        <translation>このファイルから設定を読み込む</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="61"/>
+        <source>print debug information</source>
+        <translation>デバッグ情報を表示する</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="63"/>
+        <source>do not start as daemon</source>
+        <translation>デーモンとして実行しない</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="65"/>
+        <source>force replace the existing session</source>
+        <translation>既存のセッションを強制的に置き換える</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="67"/>
+        <source>force restore the existing session</source>
+        <translation>既存のセッションを強制的に復帰させる</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="69"/>
+        <source>do not read user configuration, system-wide only</source>
+        <translation>ユーザの設定ファイルではなくシステム全体の設定ファイルのみを読み込む</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="70"/>
+        <source>Show messages:</source>
+        <translation>メッセージを表示：</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="72"/>
+        <source>show version and exit</source>
+        <translation>バージョンを表示して終了</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="74"/>
+        <source>show build information and exit</source>
+        <translation>ビルド情報を表示して終了</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="76"/>
+        <source>show this help and exit</source>
+        <translation>このヘルプを表示して終了</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="86"/>
+        <source>Build date: %1</source>
+        <translation>ビルド日時：%1</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="89"/>
+        <source>cmake flags</source>
+        <translation>cmakeのフラグ</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="91"/>
+        <source>cmake properties</source>
+        <translation>cmakeのプロパティ</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="95"/>
+        <source>Components</source>
+        <translation>コンポーネント</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="102"/>
+        <source>Additional components</source>
+        <translation>追加のコンポーネント</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="106"/>
+        <source>Project properties</source>
+        <translation>プロジェクトのプロパティ</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="112"/>
+        <source>DBus configuration</source>
+        <translation>DBus設定</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="121"/>
+        <source>Documentation</source>
+        <translation>ドキュメンテーション</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="133"/>
+        <source>Version</source>
+        <translation>バージョン</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="136"/>
+        <source>Author</source>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/messages.cpp" line="139"/>
+        <source>License</source>
+        <translation>ライセンス</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/main.cpp" line="95"/>
+        <source>The helper is running with EUID %1. Some functions may not be available.</source>
+        <translation>このヘルパはEUID %1 として実行されています。いくつか利用できない機能が存在します。</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/main.cpp" line="97"/>
+        <source>See security notes for more details.</source>
+        <translation>詳細を得るためにセキュリティノートを表示する</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/main.cpp" line="136"/>
+        <location filename="../../helper/src/main.cpp" line="147"/>
+        <source>Replace existing session.</source>
+        <translation>既存のセッションを置き換える</translation>
+    </message>
+    <message>
+        <location filename="../../helper/src/main.cpp" line="141"/>
+        <location filename="../../helper/src/main.cpp" line="153"/>
+        <source>Restore existing session.</source>
+        <translation>既存のセッションを復帰させる</translation>
+    </message>
+</context>
+</TS>
